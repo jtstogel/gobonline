d49052b028beaf90b9c4ec848e97ef32899deb70
jtstogel <jtstogel@berkeley.edu>
2024-11-11 18:54:22 -0800

    Add OpenCV dependency and some test photos (#2)
    * Add test images for reading Goban state
    
    * Include opencv in bazel deps and a test that it builds.
    
    ---------
    
    Co-authored-by: Jackson Stogel <jtstogel@jts.jacksonstogel>

---
+++ src/opencv_build/opencv_build_test.cc
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "opencv2/core/mat.hpp"
+#include "opencv2/imgcodecs.hpp"
+
+TEST(OpenCV, ImageRead) {
+  cv::Mat img = cv::imread(
+      "src/testdata/empty_rotated_dim_obstructed_board.jpeg", cv::IMREAD_COLOR);
+  cv::Size size = img.size();
+
+  EXPECT_EQ(size.width, 1000);
+  EXPECT_EQ(size.height, 750);
+}
