17cec00e4c442f5c6b21cbcb20d14279b64c4718
jackson <jtstogel@berkeley.edu>
2024-12-01 19:35:16 -0800

    fmt

--- src/util/status_test_utils.h
+++ src/util/status_test_utils.h
@@ -4,7 +4,7 @@
   auto TMPVAR(status) = (s); \
   ASSERT_TRUE(TMPVAR(status).ok()) << TMPVAR(status).status();
 
-#define ASSERT_OK_AND_ASSIGN(decl, s)                           \
-  auto TMPVAR(status) = s;                                      \
+#define ASSERT_OK_AND_ASSIGN(decl, s)                          \
+  auto TMPVAR(status) = s;                                     \
   ASSERT_TRUE(TMPVAR(status).ok()) << TMPVAR(status).status(); \
   decl = ::std::move(TMPVAR(status).value());
