f2fb5119ea4eafab70f966eade89af0d7608a07b
jackson <jtstogel@berkeley.edu>
2024-12-01 19:47:07 -0800

    use TMPVAR macro correctly

--- src/util/status.h
+++ src/util/status.h
@@ -3,16 +3,16 @@
 /**
  * Returns the expressions status if it is an error.
  */
-#define RETURN_IF_ERROR(status_expression) \
-  auto IMPL_TMPVAR = status_expression;    \
-  if (!IMPL_TMPVAR.ok()) return IMPL_TMPVAR.status()
+#define RETURN_IF_ERROR(status_expression)   \
+  auto TMPVAR(statusor) = status_expression; \
+  if (!TMPVAR(statusor).ok()) return TMPVAR(tmp).status()
 
 /**
  * Assigns the return value of `status_expression` to `decl`,
  * if the status is OK. Else, returns the error status.
  */
-#define ASSIGN_OR_RETURN(decl, status_expression)              \
-  auto IMPL_TMPVAR = status_expression;                        \
-  /** There's probably a way to avoid default initializing. */ \
-  if (!IMPL_TMPVAR.ok()) return IMPL_TMPVAR.status();          \
-  decl = ::std::move(IMPL_TMPVAR.value());
+#define ASSIGN_OR_RETURN(decl, status_expression)               \
+  auto TMPVAR(statusor) = status_expression;                    \
+  /** There's probably a way to avoid default initializing. */  \
+  if (!TMPVAR(statusor).ok()) return TMPVAR(statusor).status(); \
+  decl = ::std::move(TMPVAR(statusor).value());
